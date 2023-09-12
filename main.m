72a22ba2b3f4cf2176fac8c34cad634cc6a49032
HuangZi-zi <rayfilm@foxmail.com>
2023-09-12 15:59:33 +0800

    0912.15:59

--- main.m
+++ main.m
@@ -8,5 +8,7 @@ hist=u_histogram(imgGray);
 % 
 canny=u_find_edge(processed);
 % 
- u_find_lane(canny,hist)
+[Lane_L_X, Lane_R_X, Lane_Y]=u_find_lane(canny,hist);
+
+u_fit(Lane_L_X, Lane_R_X, Lane_Y, img);
 
---
+++ u_fit.m
@@ -0,0 +1,31 @@
+function [] = u_fit(Lane_L_X, Lane_R_X, Lane_Y, inputIMG)
+
+% x_L=a_L * x.^2 + b_L * x + c_L;
+% x_R=a_R * x.^2 + b_R * x + c_R;
+
+% 使用 polyfit 进行二次函数拟合
+degree = 2; % 拟合的多项式阶数
+coefficients_L = polyfit(Lane_Y, Lane_L_X, degree);
+coefficients_R = polyfit(Lane_Y, Lane_R_X, degree);
+
+% 从拟合系数中提取二次函数的系数
+a_L = coefficients_L(1);
+b_L = coefficients_L(2);
+c_L = coefficients_L(3);
+a_R = coefficients_R(1);
+b_R = coefficients_R(2);
+c_R = coefficients_R(3);
+
+% 创建拟合的二次函数
+Y=linspace(0,size(inputIMG,2));
+X_L=a_L * Y.^2 + b_L * Y + c_L;
+X_R=a_R * Y.^2 + b_R * Y + c_R;
+
+figure;
+imshow(inputIMG);
+hold on;
+title('二次函数拟合');
+plot(X_L, Y, 'r', 'LineWidth', 2, 'DisplayName', 'Left Lane'); % 绘制拟合曲线
+plot(X_R, Y, 'r', 'LineWidth', 2, 'DisplayName', 'Right Lane');
+
+end
