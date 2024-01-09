058ffb62171a823f9f3f7a22fc41b462605a429c
HuangZi-zi <rayfilm@foxmail.com>
2024-01-09 22:06:33 +0800

    0109.regiongrowing with depth

--- draft.m
+++ draft.m
@@ -228,11 +228,120 @@
 % end
 
 %% 
-rng('default') % For reproducibility
-X = rand(10,3).*10;
-tree = linkage(X,'average');
-c=cluster(tree,'MaxClust',5);
-figure()
-scatter3(X(:,1),X(:,2),X(:,3),10,c)
-figure()
-dendrogram(tree)
+% rng('default') % For reproducibility
+% X = rand(10,3).*10;
+% tree = linkage(X,'average');
+% c=cluster(tree,'MaxClust',5);
+% figure()
+% scatter3(X(:,1),X(:,2),X(:,3),10,c)
+% figure()
+% dendrogram(tree)
+
+%% 调用kinect
+
+addpath('C:\Users\YawnFun\Documents\project\Kin2\Mex');
+clear
+close all
+
+% Create Kinect 2 object and initialize it
+% Available sources: 'color', 'depth', 'infrared', 'body_index', 'body',
+% 'face' and 'HDface'
+k2 = Kin2('color','depth');
+i=1;
+% images sizes
+depth_width = 512; depth_height = 424; outOfRange = 4096;
+color_width = 1920; color_height = 1080;
+
+% Color image is to big, let's scale it down
+colorScale = 1;
+
+% Create matrices for the images
+depth = zeros(depth_height,depth_width,'uint16');
+color = zeros(color_height*colorScale,color_width*colorScale,3,'uint8');
+depthColor = zeros(depth_height,depth_width,3,'uint8');
+
+% depth stream figure
+% figure, h1 = imshow(depth,[0 outOfRange]);
+% title('Depth Source (press q to exit)')
+% colormap('Jet')
+% colorbar
+% set(gcf,'keypress','k=get(gcf,''currentchar'');'); % listen keypress
+
+% color stream figure
+figure, h2 = imshow(color,[]);
+title('Color Source (press q to exit)');
+set(gcf,'keypress','k=get(gcf,''currentchar'');'); % listen keypress
+
+% depthColor stream figure
+figure, h4 = imshow(depthColor,[]);
+title('Color2DephAligned (press q to exit)');
+set(gcf,'keypress','k=get(gcf,''currentchar'');'); % listen keypress
+
+% Loop until pressing 'q' on any figure
+k=[];
+
+disp('Press q on any figure to exit')
+
+while true
+    % Get frames from Kinect and save them on underlying buffer
+    validData = k2.updateData;
+    
+    % Before processing the data, we need to make sure that a valid
+    % frame was acquired.
+    if validData
+        % Copy data to Matlab matrices        
+        depth = k2.getDepth;
+        color = k2.getColor;
+        depthColor = k2.getAlignColor2Depth;               
+        
+        % update depth figure
+%         depth(depth>outOfRange) = outOfRange; % truncate depht
+%         set(h1,'CData',depth); 
+
+        % update color figure
+        color = imresize(color,colorScale);
+        set(h2,'CData',color); 
+       
+        set(h4,'CData',depthColor);
+        % u_plane_regiongrowing(color,depth);
+    end
+    
+    
+    % If user presses 'q', exit loop
+    if ~isempty(k)
+        if strcmp(k,'s')
+            imwrite(color,"Resource\snapc"+i+".png");
+            imwrite(depthColor,"Resource\snapcd"+i+".png");
+            imwrite(depth.*16,"Resource\snapd"+i+".png");
+
+            disp("snapshot");
+            i=i+1;
+            k=[];
+        elseif strcmp(k,'q')
+            break
+        end
+        
+    end
+  
+    pause(0.02)
+end
+
+% Close kinect object
+k2.delete;
+
+close all;
+
+%% 测试坐标系的相互映射关系
+addpath('C:\Users\YawnFun\Documents\project\Kin2\Mex');
+clear
+close all
+k2 = Kin2('color','depth');
+
+x=(1:600)';
+y=(1:600)';
+% x1=[1;512];
+% y1=[1;424];
+% y=ones(30,1).*20;
+
+% ans1=k2.mapDepthPoints2Color([x,y]);
+ans2=k2.mapColorPoints2Depth([300,800]);
--- u_plane_regiongrowing.m
+++ u_plane_regiongrowing.m
@@ -1,8 +1,10 @@
-function [outputArg1,outputArg2] = u_plane_regiongrowing(img_color, img_depth)
+function [outputArg1,outputArg2] = u_plane_regiongrowing(img_color, img_depth, kinect)
 
+img_color=imcrop(img_color,[0,0,512,370]);
+figure; imshow(img_color);
 [M,N,channel]=size(img_color);
 % 720,1080
-nodesize=20;
+nodesize=10;
 node_m=floor(M/nodesize);
 node_n=floor(N/nodesize);
 node_mid=floor(node_n/2);
@@ -27,7 +29,7 @@ end
 % 先遍历最下面一行
 node=nodes{node_m,node_mid};
 color_mid=reshape(mean(node,[1,2]),1,3);
-for j=1:node_n/2-1
+for j=1:floor(node_n/2-1)
     node_left=nodes{node_m,node_mid-j};
     node_right=nodes{node_m,node_mid+j};
     color_left=reshape(mean(node_left,[1,2]),1,3);
@@ -36,12 +38,20 @@ for j=1:node_n/2-1
     right=colorangle(color_right,color_mid);
     if left<t_color
         nodes{node_m,node_mid-j}=uint8(repmat(reshape([255,0,0],1,1,3),nodesize,nodesize));
+        if j==floor(node_n/2-1)
+            edges(node_m,1)=node_mid-j;
+            left_stop=j;
+        end
     elseif ~left_stop
         edges(node_m,1)=node_mid-j;
         left_stop=j;
     end
     if right<t_color&&~right_stop
         nodes{node_m,node_mid+j}=uint8(repmat(reshape([255,0,0],1,1,3),nodesize,nodesize));
+        if j==floor(node_n/2-1)
+            edges(node_m,2)=node_mid+j;
+            right_stop=j;
+        end
     elseif ~right_stop
         edges(node_m,2)=node_mid+j;
         right_stop=j;
@@ -64,7 +74,7 @@ for i=1:node_m-1
     right=colorangle(color_right,color_mid);
 
     if left<t_color% 最左侧与中心相似，向左侧扩展
-        while left<t_color
+        while left<t_color && left_stop<node_n/2-1
             left_stop=left_stop+1;
             node_left=nodes{node_m-i,node_mid-left_stop+1};
             color_left=reshape(mean(node_left,[1,2]),1,3);
@@ -73,7 +83,7 @@ for i=1:node_m-1
         nodes{node_m-i,node_mid-left_stop+1}=uint8(repmat(reshape([0,255,0],1,1,3),nodesize,nodesize));
         edges(node_m-i,1)=node_mid-left_stop+1;
     else% 最左侧与中心不相似，向右侧扩展
-        while left>t_color
+        while left>t_color && left_stop>0
             left_stop=left_stop-1;
             node_left=nodes{node_m-i,node_mid-left_stop+1};
             color_left=reshape(mean(node_left,[1,2]),1,3);
@@ -84,7 +94,7 @@ for i=1:node_m-1
     end
 
     if right<t_color% 最右侧与中心相似，向右侧扩展
-        while right<t_color&&node_mid+right_stop-1<node_n
+        while right<t_color && right_stop<node_n/2
             right_stop=right_stop+1;
             node_right=nodes{node_m-i,node_mid+right_stop-1};
             color_right=reshape(mean(node_right,[1,2]),1,3);
@@ -93,7 +103,7 @@ for i=1:node_m-1
         nodes{node_m-i,node_mid+right_stop-1}=uint8(repmat(reshape([0,255,0],1,1,3),nodesize,nodesize));
         edges(node_m-i,2)=node_mid+right_stop-1;
     else% 最右侧与中心不相似，向左侧扩展
-        while right>t_color
+        while right>t_color && right_stop>0
             right_stop=right_stop-1;
             node_right=nodes{node_m-i,node_mid+right_stop-1};
             color_right=reshape(mean(node_right,[1,2]),1,3);
@@ -106,17 +116,21 @@ end
 
 imshow(cell2mat(nodes));
 
-
-y=1:node_m;
-xl=edges(:,1);
+y=(1:node_m)';% y坐标以node最上边算
+xl=edges(:,1);% x坐标以node最左边算
 xr=edges(:,2);
-zl=img_depth(xl,y);
-zr=img_depth(xr,y);
+
+% x=[xl;xr];
+% y=repmat(y,2,1);
+
+zl=depth([xl,y]);
+zr=depth([xr,y]);
 
 a=(zl-zr)./(xl-xr);
 b=zl-a.*xl;
 
 xy=[];
+firstmerge=1;
 for k=2:node_m-1
     m1=[a(k-1),-1];
     m2=[a(k),-1];
@@ -127,14 +141,19 @@ for k=2:node_m-1
     sum_m=dot(m1,m2)/(norm(m1)*norm(m2))+dot(m1,m3)/(norm(m1)*norm(m3))+dot(m2,m3)/(norm(m2)*norm(m3));
     sum_n=dot(n1,n2)/(norm(n1)*norm(n2))+dot(n1,n3)/(norm(n1)*norm(n3))+dot(n2,n3)/(norm(n2)*norm(n3));
     measure(k)=(sum_m+sum_n)/6+0.5;
-    if measure(k)>t_merge
+    if firstmerge && measure(k)>t_merge 
         merged(k)=1;
-        xy=union(xy,[xl(k-1),y(k-1),xr(k-1),y(k-1);xl(k),y(k),xr(k),y(k);xl(k+1),y(k+1),xr(k+1),y(k+1)],"rows");      
+        xy=[xl(k-1),y(k-1),xr(k-1),y(k-1);xl(k),y(k),xr(k),y(k);xl(k+1),y(k+1),xr(k+1),y(k+1)];
+        firstmerge=0;
+    elseif measure(k)>t_merge && ~firstmerge
+        merged(k)=1;
+        xy=union(xy,[xl(k-1),y(k-1),xr(k-1),y(k-1);xl(k),y(k),xr(k),y(k);xl(k+1),y(k+1),xr(k+1),y(k+1)],"rows");
     end
 end
-
-for k=1:size(xy,2)
-    resultImage = insertShape(img_color, 'Line', xy(k), 'Color', 'blue', 'LineWidth', nodesize);
+xy=(xy-1).*nodesize+1;
+resultImage=img_color;
+for k=1:size(xy,1)
+    resultImage = insertShape(resultImage, 'Line', xy(k,:), 'Color', 'blue', 'LineWidth', nodesize);
 end
-imshow(resultImage);
+figure; imshow(resultImage);
 end
